653b3849305d0c4998974be1b9a2e59f5d0a65f8
tyanxie <494966054@qq.com>
2023-04-09 12:05:51 +0800

    新增工具函数

--- tutils.go
+++ tutils.go
@@ -3,6 +3,7 @@ package tutils
 import (
 	"encoding/json"
 	"io"
+	"os"
 )
 
 // Close 静默批量关闭
@@ -14,8 +15,25 @@ func Close(closers ...io.Closer) {
 	}
 }
 
+// Remove 静默批量删除文件
+func Remove(names ...string) {
+	for _, name := range names {
+		if name != "" {
+			_ = os.Remove(name)
+		}
+	}
+}
+
 // Json 将v使用json.Marshal转换，忽略错误
 func Json(v interface{}) string {
 	j, _ := json.Marshal(v)
 	return string(j)
+}
+
+// Ternary 三元表达式，当condition成立时返回x，否则返回y
+func Ternary[T interface{}](condition bool, x, y T) T {
+	if condition {
+		return x
+	}
+	return y
 }
